b402702946440c26084147b6d21c6e4cc235d0c6
Benjamin-Connelly <13756101+Benjamin-Connelly@users.noreply.github.com>
2024-03-14 08:40:09 -0600

    updated chocolately install

--- Vagrant/scripts/install-choco-extras.ps1
+++ Vagrant/scripts/install-choco-extras.ps1
@@ -4,7 +4,7 @@ Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Installing additional Choco packages.
 
 If (-not (Test-Path "C:\ProgramData\chocolatey")) {
   Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Installing Chocolatey"
-  iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+  Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 } else {
   Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Chocolatey is already installed."
 }
